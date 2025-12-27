8d15e38e24c5c789e4e8dd60cdef62bb031aa0f4
Arina Zubova <arina777pototskaya@gmail.com>
2025-12-27 17:26:26 +0300

    init commit

---
+++ src/test/java/io/github/bulbaattacks/log_sender/LogSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bulbaattacks.log_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
